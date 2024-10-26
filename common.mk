84b22bcdc0906ccbc89cb7bb3fef285e864d854e
ahnet-69 <syedmusicrealise@gmail.com>
2024-10-26 15:15:12 +0000

    sm6225-common: Set PRODUCT_BROKEN_VERIFY_USES_LIBRARIES
    * Fixes mismatch manifest libraries error

--- common.mk
+++ common.mk
@@ -389,6 +389,9 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-lite-3.9.1-vendorcompat \
     librmnetctl
 
+# Rules
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal \
